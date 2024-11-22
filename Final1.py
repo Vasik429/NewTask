893bbaa1db05f57fe7ff57308b7dcea6e652e4f5
Vasik429 <v.404.akk@gmail.com>
2024-11-23 03:43:31 +0500

    Доп задание модуль1

---
+++ Final1.py
@@ -0,0 +1,20 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+# print(type(grades))
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+# print(type(students))
+
+for students in grades:
+    grades[0] = round(sum([5, 3, 3, 5, 4]) / len ([5, 3, 3, 5, 4]), 2)
+    grades[1] = round(sum([2, 2, 2, 3]) / len([2, 2, 2, 3]), 2)
+    grades[2] = round(sum([4, 5, 5, 2]) / len([4, 5, 5, 2]), 2)
+    grades[3] = round(sum([4, 4, 3]) / len([4, 4, 3]), 2)
+    grades[4] = round(sum([5, 5, 5, 4, 5]) / len([5, 5, 5, 4, 5]), 2)
+print(grades)
+
+students = list({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'})
+# print(type(students))
+students.sort(key=None, reverse=False)
+print(students)
+
+mark = dict(zip(students, grades))
+print(mark)
