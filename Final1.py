f773d4e79d1c2f0540628c2290387e8df87176f0
Vasik429 <v.404.akk@gmail.com>
2024-11-26 15:35:59 +0500

    Убрала цикл

--- Final1.py
+++ Final1.py
@@ -1,18 +1,17 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 # print(type(grades))
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-# print(type(students))
+print(type(students))
 
-for students in grades:
-    grades[0] = round(sum([5, 3, 3, 5, 4]) / len ([5, 3, 3, 5, 4]), 2)
-    grades[1] = round(sum([2, 2, 2, 3]) / len([2, 2, 2, 3]), 2)
-    grades[2] = round(sum([4, 5, 5, 2]) / len([4, 5, 5, 2]), 2)
-    grades[3] = round(sum([4, 4, 3]) / len([4, 4, 3]), 2)
-    grades[4] = round(sum([5, 5, 5, 4, 5]) / len([5, 5, 5, 4, 5]), 2)
+grades[0] = round(sum([5, 3, 3, 5, 4]) / len ([5, 3, 3, 5, 4]), 2)
+grades[1] = round(sum([2, 2, 2, 3]) / len([2, 2, 2, 3]), 2)
+grades[2] = round(sum([4, 5, 5, 2]) / len([4, 5, 5, 2]), 2)
+grades[3] = round(sum([4, 4, 3]) / len([4, 4, 3]), 2)
+grades[4] = round(sum([5, 5, 5, 4, 5]) / len([5, 5, 5, 4, 5]), 2)
 print(grades)
 
 students = list({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'})
-# print(type(students))
+print(type(students))
 students.sort(key=None, reverse=False)
 print(students)
 
