9c07f219168dd2614bb1b0d86df46bd49ff20e69
Vasik429 <v.404.akk@gmail.com>
2024-11-26 21:09:41 +0500

    Работа над ошибками

--- Final1.py
+++ Final1.py
@@ -3,17 +3,12 @@ print(type(grades))
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 print(type(students))
 
-grades[0] = round(sum([5, 3, 3, 5, 4]) / len ([5, 3, 3, 5, 4]), 2)
-grades[1] = round(sum([2, 2, 2, 3]) / len([2, 2, 2, 3]), 2)
-grades[2] = round(sum([4, 5, 5, 2]) / len([4, 5, 5, 2]), 2)
-grades[3] = round(sum([4, 4, 3]) / len([4, 4, 3]), 2)
-grades[4] = round(sum([5, 5, 5, 4, 5]) / len([5, 5, 5, 4, 5]), 2)
+i = 0
+while i < len(grades):
+    grades[i] = round(sum(grades[i]) / len(grades[i]), 2)
+    i += 1
 print(grades)
 
-students = list({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'})
-print(type(students))
-students.sort(key=None, reverse=False)
-print(students)
-
-mark = dict(zip(students, grades))
+people = sorted(students)
+mark = dict(zip(people, grades))
 print(mark)
